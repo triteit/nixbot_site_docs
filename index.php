4fe7046bb003d785eae16bde9176a197e63cfcb7
triteit <95466401+triteit@users.noreply.github.com>
2023-06-07 21:54:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<?php
+$uri = $_SERVER['REQUEST_URI'];
+$extension = pathinfo($uri, PATHINFO_EXTENSION);
 
+// Проверяем, является ли расширение .html
+if ($extension === 'html') {
+    // Удаляем расширение из адресной строки
+    $uri = preg_replace('/\.html$/', '', $uri);
+    
+    // Перенаправляем пользователя на новый URL без расширения
+    header("Location: $uri", true, 301);
+    exit();
+}
